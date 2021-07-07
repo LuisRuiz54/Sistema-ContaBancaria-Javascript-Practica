b123e241530c7d1c8c1a221f7334f367aefb355f
Luis Ruiz <nessiuscaffe@gmail.com>
2021-07-07 11:33:23 -0300

    Aplicando encapsulamentos de classes e protegendo Atributos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,10 +1,26 @@
+import { Cliente } from "./Cliente.js";
 
-export class ContaCorrente{
+export class ContaCorrente {
     agencia;
-    cliente;
+    _cliente;
 
     _saldo = 0;
-    
+
+     get saldo() {
+         return this._saldo;
+     }  
+
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
--- index.js
+++ index.js
@@ -15,14 +15,14 @@ contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
+
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
 let valor = 200;
 contaCorrenteRicardo.tranferir(valor, conta2);
 
-console.log("valor: ", valor)
-console.log(conta2);
-
+console.log(conta2.cliente);
+console.log(conta2.saldo);
 
