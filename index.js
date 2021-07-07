3d26034bb150065d57ba71e2c21f060d3645286c
Luis Ruiz <nessiuscaffe@gmail.com>
2021-07-07 01:25:57 -0300

    Criando Modulos separados ContaCorrente e Clientes

--- index.js
+++ index.js
@@ -1,62 +1,28 @@
-class Cliente {
-    nome;
-    cpf;
-   
-}
-
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-
-        }
-        return
-    }
-
-    depositar(valor) {
-        if(valor <= 0) {
-           return;       
-        }
-        this._saldo += valor;
-    }
-}
-
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
 const cliente2 = new Cliente();
-const cliente3 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-const valorSacado =  contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
 
-console.log(contaCorrenteRicardo);
-
-
-cliente1.nome = "Ricardo";
-cliente1.cpf = 64831777390;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+console.log("valor: ", valor)
+console.log(conta2);
 
 
-cliente2.nome = "Maria";
-cliente2.cpf = 213154731;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
-
-
-cliente3.nome = "Ana";
-cliente3.cpf= 6483135417390;
-cliente3.agencia = 1001;
-cliente3.saldo = 0;
